209e759e2a8d70945fc59d684695445550b00b26
kelvinpraises <kelvinpraises@gmail.com>
2022-08-25 20:46:54 +0100

    refactored app

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-import 'settings/settings_controller.dart';
-import 'settings/settings_view.dart';
+import 'features/settings/settings_controller.dart';
+import 'features/settings/settings_view.dart';
 
 /// The Widget that configures your application.
 class MyApp extends StatelessWidget {
